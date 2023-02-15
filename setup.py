32abeabf92533915f8716731c9f1cde2afcc7e19
Dodo11742 <kabitamsahoo@gmail.com>
2023-02-15 16:31:55 -0500

    finished packaging, released to pypi

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.4'
 DESCRIPTION = 'A MIDI file analyzer with a few additional functionality'
-LONG_DESCRIPTION = 'A MIDI file analyzer which allows you to go through components of a MIDI file and extract data. Also has functionality such as generating pdfs'
+LONG_DESCRIPTION = 'A MIDI file analyzer which allows you to go through components of a MIDI file and extract data. Also has functionality such as generating pdfs <br><br> For documentation, go to [swayamsahoo11742.github.io](https://swayamsahoo11742.github.io/) <br><br> For Github page, go to [https://github.com/SwayamSahoo11742/Scopul](https://github.com/SwayamSahoo11742/Scopul) '
 
 # Setting up
 setup(
@@ -19,12 +19,12 @@ setup(
     author_email="<swayamsa01@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=['music21', 'mido'],
     keywords=['python', 'scopul', 'MIDI', 'sheet music', 'midi anazyler', 'music'],
     classifiers=[
-        "Development Status :: 1 - Early Life",
+        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
