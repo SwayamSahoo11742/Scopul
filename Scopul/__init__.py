070cccd6f3760b4caaab2eebf117a6e643cc4108
SwayamSahoo11742 <swayamsa01@gmail.com>
2023-03-14 20:30:12 -0400

    added tests for most functions and classes, more info on contributing coming soon

--- Scopul/__init__.py
+++ Scopul/__init__.py
@@ -6,4 +6,4 @@ from Scopul.Tempo import Tempo
 from Scopul.Sequence import Part, Note, Rest, Chord
 from Scopul.conversions import note_to_number, number_to_note
 from Scopul.config_musescore import config_musescore
-# Imports
+# Imports for scopul
